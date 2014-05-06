1099aa3e84de85435f61d352873a2926efefc964
Jon <apps2black@gmail.com>
2014-05-06 23:31:39 +0000

    Adjust production.rb so it compiles assets

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Nomster::Application.config.secret_key_base = '799e8e54896add2400983949863c231bed32ad33d430cbdf6cda6c1c8302f56cbb868d1da9f3ebba2b0eeb7a9f7fbea68f531e174a8cc8069bc53572a07ce4e4'
