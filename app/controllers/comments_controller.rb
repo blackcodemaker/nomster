e3ec9f78f9f9e3620d9affe944838b76af14f242
Jonathan Black <apps2black@gmail.com>
2014-05-27 09:39:34 -0700

    Added mailer

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,12 +1,14 @@
 class CommentsController < ApplicationController
 	before_action :authenticate_user!
 	def create
-		@place.Place.find(params[:place_id])
+		@place=Place.find(params[:place_id])
 		@place.comments.create(comment_params.merge(:user => current_user))
-		redrect_to place_path(@place)
+		redirect_to place_path(@place)
 	end
 
 	private
 
-	params.require(:comment).permit(:message, :rating)
+	def comment_params
+		params.require(:comment).permit(:message, :rating)
+	end
 end
