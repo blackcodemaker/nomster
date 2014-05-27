ff019e93ad835726c76f7f3518271d24f6c11312
Jonathan Black <apps2black@gmail.com>
2014-05-27 10:07:26 -0700

    Tested notifications

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -2,6 +2,6 @@ class NotificationMailer < ActionMailer::Base
   default from: "no-reply@nomsterapp.com"
 
   def comment_added
-  	mail(to: "jblack4546@hotmail.com", subject: "A comment has been added to your place.")
+  	mail(to: "apps2black@gmail.com", subject: "A comment has been added to your place.")
   end
 end
